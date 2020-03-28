5cc1f75b687ec92cdfa21d3af9fbaca30855ec4d
findridoy <findridoy@gmail.com>
2020-03-28 16:38:38 +0600

    nav drawer

---
+++ lib/nav_drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: null,
+      child: Text("Tap me"),
+    );
+  }
+}
