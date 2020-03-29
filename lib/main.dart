0b10254e2f23fc9ac789878d4935b249db005a24
findridoy <findridoy@gmail.com>
2020-03-29 16:46:39 +0600

    progres

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
@@ -12,12 +11,7 @@ class BTIweb extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'বাহাউল হক টেকনিক্যাল ইন্সটিটিউট - Bahaul Huq Technical Institute',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hey"),
-        ),
-        body: HomePage(),
-      ),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/carousel/gf_carousel.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        title: Center(
+          child: FittedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network("https://via.placeholder.com/60"),
+                Text("bahaul kdjfei kdsjf lkjdf")
+              ],
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          MenuButton(),
+        ],
+        automaticallyImplyLeading: false,
+      ),
+      endDrawer: Container(
+        width: 50,
+        color: Colors.amber,
+        child: ListView(
+          children: <Widget>[
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+          ],
+        ),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            WelcomeCarousel(),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+            Text("Hi"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: () => Scaffold.of(context).openEndDrawer(),
+      ),
+    );
+  }
+}
+
+class WelcomeCarousel extends StatelessWidget {
+  final List<String> imageList = [
+    "assets/welcome_image/img1",
+    "assets/welcome_image/img2",
+    "assets/welcome_image/img3",
+    "assets/welcome_image/img4",
+    "assets/welcome_image/img5",
+    "assets/welcome_image/img6",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GFCarousel(
+        items: imageList.map((img) {
+          return Container(
+            child: Image.asset(
+              img,
+              fit: BoxFit.cover,
+              width: 1000,
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
